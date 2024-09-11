c04bc4be2f841e6ddd6e0168d234a4b99f4209f9
James <jamespsteinberg@gmail.com>
2024-09-11 15:11:29 -0400

    bump version

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(name='delete-your-pdf',
-    version='1.0.0',
+    version='1.0.2',
     description='Crop, Rotate, and extract text from your PDFs so you can delete them',
     author='James Steinberg',
     author_email='jamespsteinberg@gmail.com',
     url='https://github.com/DareFail/delete-your-pdf',
     packages=['deleteYourPDF'],
     install_requires=['pypdfium2', 'pillow'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
